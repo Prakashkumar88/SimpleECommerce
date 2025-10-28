253b1b0addad87739c1ab39c1c4f9f2880ff2d46
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2025-10-28 18:50:19 +0530

    Delete CalculatorApp directory

--- CalculatorApp/src/main/java/org/example/calculator/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.calculator.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "org.example.calculator")
-public class AppConfig {
-}
--- CalculatorApp/src/main/java/org/example/calculator/service/Operation.java
+++
@@ -1,5 +0,0 @@
-package org.example.calculator.service;
-
-public interface Operation {
-    double operate(double a, double b);
-}
